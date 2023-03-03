e76ce5ffae85a8a68630dd7a3fd0c8896b405689
MO <mo.tech.life20@gmail.com>
2023-03-03 17:04:00 -0500

    completed user token authentication

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -45,9 +45,13 @@ exports.login = (req, res, next) => {
               error: new Error("Incorrect password!"),
             });
           }
+          const token = jwt.sign(
+            {userId: user._id}, 
+            'RANDOM_TOKEN_SECRET',
+            {expiresIn: '24h'});
           res.status(200).json({
             userId: user._id,
-            token: "token",
+            token: token
           });
         })
         .catch((error) => {

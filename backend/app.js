2255996f7314c72c7b43333acc581373477cf780
MO <mo.tech.life20@gmail.com>
2023-03-03 20:36:21 -0500

    corrected sauce model; created middleware directory; completed auth and multer-config files; start working on sauce route

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config({path: '../.env'});
 
+const Sauce = require('./models/sauce');
+
 const userRoutes = require('./routes/user');
 
 const app = express();
@@ -24,6 +26,51 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+
+app.post('/api/sauces', (req, res, next) => {
+  const sauce = new Sauce({
+    sauce: String,
+    image: File,
+  });
+  sauce
+    .save()
+    .then(() => {
+      res.status(201).json({
+        message: "Sauce added successfully!",
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+});
+
+app.get("/api/sauces/:id", (req, res, next) => {
+  Sauce.findOne({
+    _id: req.params.id,
+  })
+    .then((sauce) => {
+      res.status(200).json(sauce);
+    })
+    .catch((error) => {
+      res.status(404).json({
+        error: error,
+      });
+    });
+});
+
+app.get('/api/sauces', (req, res, next) => {
+  Sauce.find()
+    .then((sauces) => {
+      res.status(200).json(sauces);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+});
 
 app.use('/api/auth', userRoutes);
 
--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
-    userId: { type: String, required: true },
-    name: { type: String, required: true },
-    manufacturer: { type: String, required: true },
-    description: { type: String, required: true },
-    mainPepper: {type: String, required: true},
-    imageUrl: {type: String, required: true},
-    heat: {type: Number, required: true},
-    likes: {type: Number, required: true},
-    dislikes: {type: Number, required: true},
-    userLiked: [{userID, required: true}],
-    userDisliked: [{userID, required: true}]
+    userId: String, 
+    name: String, 
+    manufacturer: String, 
+    description: String,
+    mainPepper: String,
+    imageUrl: String, 
+    heat: Number,
+    likes: Number, 
+    dislikes: Number, 
+    userLiked: [{userId: String}],
+    userDisliked: [{userId: String}]
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
